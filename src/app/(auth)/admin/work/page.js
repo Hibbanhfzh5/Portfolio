6b83df2a247266fdaa35f9376cf3c0a9d91bd6e6
hibbanhfzh5 <hibbanhafizh0@gmail.com>
2024-10-07 21:08:40 +0700

    7/10/2024

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -1,77 +1,131 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useState } from 'react';
+import Card from '../../../../components/card';
 
-const ItemCard = ({label, value})=>{
-    return (
-        <div className='flex gap-4 bg-white  rounded-md m-2 p-2'>
-            <div>{ label }</div>
-            <div>{ value }</div>
-        </div>
-    )
-    
-}
+export default function AdminWork() {
+  const [data, setData] = useState({
+    title:'',
+    employeType:'',
+    companyName:'',
+    location:'',
+    startDate:'',
+    endDate:'',
+  });
+  
+  const optEmployeType = [
+    {label:'Full Time', value:'full-time'},
+    {label:'Part Time', value:'part-time'},
+    {label:'Contract', value:'contract'},
+    {label:'Internship', value:'internship'}
+  ]
 
-export default function Adminwork() {
-    const [data, setData] = useState([])
-    const [isLoading, setLoading] = useState(true)
+  const optLocation = [
+    {label:'Onsite', value:'onsite'},
+    {label:'WFH', value:'wfh'},
+  ]
 
-    async function onLoadData() {
-        setLoading(true)
-        let res = await fetch('/api/contact')
-        let data = await res.json()
-        setData(data)
-        setLoading(false)
+  const inputHandler= (e) =>{
+    setData({...data, [e.target.name]: e.target.value })
+  }
+
+  async function onSubmitData() {
+    try{
+      let res = await fetch('/api/work', {
+        method:'POST',
+        body: JSON.stringify(data),
+      })
+      let resData = await res.json()
+      if(!resData.data){
+        throw Error(resData.message)
+      }
+      alert("Data berhasil disimpan dengan id \n"+ resData.data.insertedId)
+    }catch(err){
+      console.error("ERR", err.message)
+      alert(err.message)
     }
-
-    useEffect(() => {
-        onLoadData()
-    }, [])
-
-   return (
-        <> 
-            <h2 className="text-center text-3xl w-full">Get In Touch</h2>
-            
-            <p className="text-center margin-0 mx-auto w-2/3	">
-                lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem 
-                ipsum lorem ipsum lorem ipsum 
-            </p>
-
-            <div className="flex mt-16 place-content-center">
-                <div className="md:w-3/4 px-4 ">
-                    <div className=" bg-white p-10  rounded-xl">
-                        <h3 className="text-2xl py-2">Add Experience</h3>   
-                        <div className="flex gap-3">
-                        <div className="w-full my-2">
-                            <label>Title</label>
-                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ex: Retail Sales Manager" type="text" name="search"/>
-                        </div>
-                        <div className="w-full my-2">
-                            <label>Employment type</label>
-                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Please select" type="text" name="search"/>
-                        </div>
-                        </div>
-                        <div className="w-full my-2">
-                            <label>Company Name</label>
-                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ex: Microsoft" type="text" name="search"/>
-                        </div>
-                        <div className="w-full">
-                            <label>Location</label>
-                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ex: London, United Kingdom" type="text" name="search"/>
-                        </div>
-                        <div className="w-full">
-                            <label>Location Type</label>
-                            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Please Select" type="text" name="search"/>
-                        </div>
-                        <div className="w-full py-2">
-                            <button className="mx-0 h-9 mt-2 items-center justify-center px-6  rounded-md bg-blue-500 text-white ">
-                                <label>Save new</label>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-               
-            </div>
-
-        </>
-    );
 }
+
+
+  return (<>
+      <Card title="Work Form" className="pb-5">
+        <div className="w-full my-2">
+            <label>Title</label> 
+            <input 
+              type="text" 
+              name='title'
+              value={data.title}
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Employe Type</label>
+            <select  
+              name='employeType' 
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optEmployeType && 
+                  optEmployeType.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Company Name</label>
+            <input 
+              name='companyName' 
+              type="text" 
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Location</label>
+            <select 
+              name='location'
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optLocation && 
+                optLocation.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
+
+        <div className="w-full my-2">
+            <label>Start Date</label>
+            <input 
+              name='startDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>End Date</label>
+            <input 
+              name='endDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <button 
+          onClick={onSubmitData}
+          className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+            <label>Submit Data</label>
+        </button>
+      </Card>
+      
+      <Card title="List of Work" style="mt-5">
+        asdf
+      </Card>
+    </>
+  );
+}
+  
